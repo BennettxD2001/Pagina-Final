1146fc732fc3c93247d4fb927a7811c5548964cb
andrea-leon <leon.acevesandrea@gmail.com>
2022-12-02 18:38:09 -0600

    cambio andrea

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let allProducts = []
 
 
 
-productsList.addEventListener('click', e => {
+productsList.addEventListener ('click', e => {
     if (e.target.classList.contains('btn-add-cart')){
         const product = e.target.parentElement
 
@@ -29,10 +29,61 @@ productsList.addEventListener('click', e => {
  
         }
         
-        allProducts = [...allProducts, infoProduct]
+        const exist = allProducts.some(product => product.title === infoProduct.title)
+
+        if(exist){
+            const products = allProducts.map(product =>{
+                if(product.title === infoProduct.title){
+                    product.quantity++;
+                    return product
+                }else 
+                return product;
+            })
+            allProducts = [...products]
+        }else{
+            allProducts = [...allProducts, infoProduct];
+        }
+
+
+        
+
+        showHTML();
     }
     console.log(allProducts)
-})
+});
 
 // mostrar HTML 
+const showHTML = () =>{
+
+//limpiar HTML
+
+rowProduct.innerHTML = '';
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div')
+        containerProduct.classList.add('cart-product')
+
+        containerProduct.innerHTML =`<div class="info-cart-product">
+                            <span class="cantidad-producto-carrito">${product.quantity}</span>
+                            <p class="titulo-producto-carrito">${product.title}</p>
+                            <span class="precio-producto-carrito">${product.price}</span>
+                        </div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="icon-close"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>`
+
+                        rowProduct.append(containerProduct)
+    })
+}
 
